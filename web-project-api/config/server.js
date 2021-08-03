e0787255a4f4e2d89bf8e0d4112ee24b57b03bb4
pirapat jitcharoenwirakun <pirapat.j@ku.th>
2021-08-03 19:11:27 +0700

    api-first-push

---
+++ web-project-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dbc9bd1530d336fc00f66c1e328379ea'),
+    },
+  },
+});
---
+++ web-project-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '634bc00f-ea3a-4b44-a35b-9010e24cb8e1'
+};
